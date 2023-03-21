d800305f274ce3817ebbe7f1950af68f652d76ea
cooldog124 <128550433+cooldog124@users.noreply.github.com>
2023-03-21 17:36:25 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,21 @@
-print("test")
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from pathfinding.core.diagonal_movement import DiagonalMovement
+#replace 1 with 0 to get more walls
+#replace 0 with 1 to delete a wall
+map = [
+[1,1,1,1,1,1],
+[1,1,0,1,1,1],
+[1,1,1,1,1,1]]
+
+grid=Grid(matrix=map)
+#change coordinates to change starting point and the end point
+start=grid.node(1,1)
+end=grid.node(5,2)
+
+finder=AStarFinder(diagonal_movement=DiagonalMovement.always)
+
+path,runs,=finder.find_path(start,end,grid)
+
+print(path)
+print(str(runs)+" runs")
